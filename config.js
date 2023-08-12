a8996263977e78c62b44580e5d9d343a9717881b
XUJF158 <130628524+XUJF158@users.noreply.github.com>
2023-08-12 09:35:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     signInPage: "https://glados.rocks/console/checkin",
   },
   headers: { "Content-Type": "application/json;charset=utf-8" },
-  body: '{"token":"glados.network"}',
+  body: '{"token":"glados.one"}',
   // 发送者
   sender: { user: email, pass: pass },
   // 接收者
